946f1f47a38410b49814cf1a9b58e2b2b44430b9
Gabriela Silva <vigabiesdivision@gmail.com>
2023-03-15 08:48:33 -0300

    create database and insert

---
+++ src/main/resources/db/migration/V001__Tabela__Ator.sql
@@ -0,0 +1,7 @@
+Create Table Ator (
+id int not null primary key auto_increment,
+nomeator varchar (100)
+)
+
+insert into ator(nomeator) values ("Terry Crews");
+
---
+++ src/main/resources/db/migration/V003__Tabela__Genero.sql
@@ -0,0 +1,9 @@
+Create Table Genero(
+id int not null auto_increment primary key,
+descricao varchar (100),
+);
+
+insert into genero(descricao) values ("Sitcom, Comedy");
+insert into genero(descricao) values ("Comedy and Crime");
+insert into genero(descricao) values ("Sitcom, Comedy and Policial Processual");
+
