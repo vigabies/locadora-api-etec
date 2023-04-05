16c003a5f2d9047bb95f346c845d40a9329d6a6e
Gabriela Silva <vigabiesdivision@gmail.com>
2023-04-05 08:27:18 -0300

    db formated

--- src/main/resources/db/migration/V001__Tabela__Genero.sql
+++ src/main/resources/db/migration/V001__Tabela__Genero.sql
@@ -1,6 +1,6 @@
 Create Table Genero(
-id int not null auto_increment primary key,
-descricao varchar (100),
+    id int not null auto_increment primary key,
+    descricao varchar (100)
 );
 
 insert into genero(descricao) values ("Sitcom, Comedy");
--- src/main/resources/db/migration/V002__Tabela__Ator.sql
+++ src/main/resources/db/migration/V002__Tabela__Ator.sql
@@ -1,7 +1,20 @@
 Create Table Ator (
-id int not null primary key auto_increment,
-nomeator varchar (100)
-)
+    id int not null primary key auto_increment,
+    nomeator varchar (100)
+);
 
 insert into ator(nomeator) values ("Terry Crews");
 
+Create Table Filmes(
+    id bigint not null primary key auto_increment,
+    nomefilme varchar (140),
+    idgenero int not null,
+    idator int not null
+);
+
+insert into Filmes(nomefilme, idgenero, idator) values ('Brooklyn Nine', 3, 1);
+insert into Filmes(nomefilme, idgenero, idator) values ('White Chicks', 2, 1);
+insert into Filmes(nomefilme, idgenero, idator) values ('Everybody hates Chris', 1, 1);
+
+Alter table Filmes add constraint FK_genero_filme foreign key (idgenero) references genero(id);
+Alter table Filmes add constraint FK_ator_filme foreign key (idator) references ator(id);
